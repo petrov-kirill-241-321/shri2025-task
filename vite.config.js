7d2178aa9b831bfbb4c411ddcb337ae764a7bc39
Kirill <kurumi8927@gmail.com>
2025-07-06 23:35:48 +0300

    18

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,10 @@ export default defineConfig({
   plugins: [react(), imagemin({ gifsicle: { optimizationLevel: 3 } })], // Обработка JSX
   base: "/shri2025-task/",
   build: {
+    outDir: "dist",
     rollupOptions: {
       input: {
-        main: "./index.html", // Указываем входной файл
+        main: resolve(__dirname, "index.html"),
       },
     },
   },
