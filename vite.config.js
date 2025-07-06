80af69847203427f7013a58c28b65dc15a772a05
Kirill <kurumi8927@gmail.com>
2025-07-06 23:38:37 +0300

    19

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import imagemin from "vite-plugin-imagemin";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), imagemin({ gifsicle: { optimizationLevel: 3 } })], // Обработка JSX
@@ -9,7 +10,7 @@ export default defineConfig({
     outDir: "dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
+        main: path.resolve(__dirname, "index.html"),
       },
     },
   },
