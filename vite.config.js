9c9af99b822bb044b12f15b11426e3b139a172d4
Kirill <kurumi8927@gmail.com>
2025-07-06 23:46:45 +0300

    21

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
 import { defineConfig } from "vite";
 import imagemin from "vite-plugin-imagemin";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react(), imagemin({ gifsicle: { optimizationLevel: 3 } })], // Обработка JSX
   base: "/shri2025-task/",
   build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"),
-      },
-    },
+    outDir: "dist", // Куда складывать сборку
+    emptyOutDir: true, // Очищать папку перед сборкой
   },
 });
