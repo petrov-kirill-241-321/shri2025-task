70f79dfe106a6ec4ffd0bad419a5c4172e335937
Kirill <kurumi8927@gmail.com>
2025-07-06 22:03:39 +0300

    2

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import imagemin from "vite-plugin-imagemin";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react(), imagemin({ gifsicle: { optimizationLevel: 3 } })], // Обработка JSX
+  base: "https://petrov-kirill-241-321.github.io/shri2025-task",
+});
